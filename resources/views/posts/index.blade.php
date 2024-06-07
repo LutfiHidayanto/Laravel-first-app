b4cc4946b13e8c6ce9f81b9b3ab852c3c46c129b
lutfiHidayanto <hajidlutfi@gmail.com>
2024-06-07 22:54:03 +0700

    bruh

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,20 +6,23 @@
     </div>
     <div class="flex justify-center items-center mb-10">
         <a href="{{ route('posts.create') }}" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Create New Post
+            Create New Post
         </a>
     </div>
-    <div class="flex-col space-y-6">
+    <div class="space-y-6">
         <ul>
             @foreach ($posts as $post)
-                <li>
-                    <a class="ms-2 text-2xl font-semibold text-gray-500 dark:text-gray-400 mr-4 hover:text-black" href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
-                    <a  class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" href="{{ route('posts.edit', $post->id) }}">Edit</a>
-                    <form action="{{ route('posts.destroy', $post->id) }}" method="POST" style="display:inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button  class="inline-block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" type="submit">Delete</button>
-                    </form>
+                <li class="flex justify-between items-center mb-4 p-4 bg-gray-100 rounded-lg shadow dark:bg-gray-800">
+                    <a class="text-2xl font-semibold text-gray-500 dark:text-gray-400 hover:text-black" href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
+                    <div class="flex space-x-2">
+                        <a class="inline-block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" href="{{ route('posts.show', $post->id) }}">View</a>
+                        <a class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" href="{{ route('posts.edit', $post->id) }}">Edit</a>
+                        <form action="{{ route('posts.destroy', $post->id) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button class="inline-block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" type="submit">Delete</button>
+                        </form>
+                    </div>
                 </li>
             @endforeach
         </ul>
